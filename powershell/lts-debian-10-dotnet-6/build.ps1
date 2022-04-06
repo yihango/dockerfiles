1cddf84f3c90f4500405f4bcf009a451bdfd5493
staneee <yi.hang@live.com>
2022-04-06 15:55:49 +0800

    修改dotnet的镜像

--- powershell/lts-debian-10-dotnet-6/build.ps1
+++ powershell/lts-debian-10-dotnet-6/build.ps1
@@ -1,2 +1,4 @@
 docker build . --force-rm -t staneee/powershell:lts-debian-10-dotnet-6  -f ./Dockerfile
 docker push staneee/powershell:lts-debian-10-dotnet-6
+docker tag staneee/powershell:lts-debian-10-dotnet-6 registry.cn-shanghai.aliyuncs.com/staneee/powershell:lts-debian-10-dotnet-6
+docker push registry.cn-shanghai.aliyuncs.com/staneee/powershell:lts-debian-10-dotnet-6
