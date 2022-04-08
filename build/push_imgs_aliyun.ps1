ac6a26bb3b536fa5f0e4e9f41c58a6c00a3d890a
staneee <yi.hang@live.com>
2022-04-08 10:22:32 +0800

    增加推送到阿里云的脚本

--- build/push_imgs_aliyun.ps1
+++ build/push_imgs_aliyun.ps1
@@ -1,4 +1,7 @@
-$imgNamespace = "staneee";
+
+
+$imgNamespace = "staneee"
+$imgNamespaceAliyun = "registry.cn-shanghai.aliyuncs.com/staeee"
 $dockerFiles = Get-ChildItem -r "./src" | Where-Object {
     $_ -is [System.IO.FileInfo] -and $_.FullName.EndsWith('Dockerfile')
 } | Select-Object -ExpandProperty FullName
@@ -18,20 +21,18 @@ foreach ($path in $dockerFiles) {
 
     # 镜像全名称
     $imgFullName = $imgNamespace + '/' + $imgName + ':' + $imgTag
+    $imgFullNameAliyun = $imgNamespaceAliyun + '/' + $imgName + ':' + $imgTag
 
     Write-Host "\r\n\r\n============= start $imgFullName =============\r\n\r\n"
 
     # 拉取
     docker pull $imgFullName
     
-    # 切换到Dockerfile所在目录
-    Set-Location $imgTagDir
-    
-    # 编译镜像
-    docker build . --force-rm -t $imgFullName  -f ./Dockerfile
+    # 重命名
+    docker tag $imgFullName $imgFullNameAliyun
 
     # 推送镜像
-    docker push $imgFullName
+    docker push $imgFullNameAliyun
 
     Write-Host "\r\n\r\n============= stop $imgFullName =============\r\n\r\n"
 
