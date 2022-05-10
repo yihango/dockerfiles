5626f89c1647bb0d2dfea6b179d035c7b8e6eb2a
staneee <yi.hang@live.com>
2022-05-10 16:45:40 +0800

    添加镜像

---
+++ src/aspnet/3x-puppeteer/build.ps1
@@ -0,0 +1,1 @@
+docker build -t staneee/aspnet:3x-puppeteer .
---
+++ src/python/3.7.13-buster-test-basic/build.ps1
@@ -0,0 +1,2 @@
+docker build . --force-rm -t staneee/python:3.7.13-buster-test-basic  -f ./Dockerfile
+docker push staneee/python:3.7.13-buster-test-basic
