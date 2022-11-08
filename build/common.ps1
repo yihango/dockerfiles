dc3f4192ac5754b55721cec74a324a9ffca37b05
staneee <yi.hang@live.com>
2022-11-08 18:25:02 +0800

    修改ci

--- build/common.ps1
+++ build/common.ps1
@@ -6,18 +6,6 @@ $needBuild = (
 )
 
 
-# arm64
-$buildXArm64 = (
-    "staneee/powershell:lts-debian-10-docker-20-10-21-arm64"
-    # "staneee/aspnet:5-gdi-fontconfig-arm64v8",
-)
-
-# amd64
-$buildXAmd64 = (
-    ""
-    # "staneee/aspnet:6-gdi-fontconfig"
-)
-
 # 创建编译器
 # docker buildx create --name mybuilder --driver docker-container --bootstrap
 # docker buildx use mybuilder
