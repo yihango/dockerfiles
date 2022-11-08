7175d5173c87b83a4dc7215c64e8e4ae41128cbc
staneee <yi.hang@live.com>
2022-11-08 15:20:46 +0800

    跳过所有

--- build/common.ps1
+++ build/common.ps1
@@ -1,14 +1,16 @@
 # 需要打包的
 $needBuild = (
-    "staneee/aspnet:5-gdi-fontconfig",
-    "staneee/aspnet:6-gdi-fontconfig"
+    ""
+    # "staneee/aspnet:5-gdi-fontconfig",
+    # "staneee/aspnet:6-gdi-fontconfig"
 )
 
 
 # 使用buildx amd64和arm64的
 $buildX = (
-    "staneee/aspnet:5-gdi-fontconfig",
-    "staneee/aspnet:6-gdi-fontconfig"
+    ""
+    # "staneee/aspnet:5-gdi-fontconfig",
+    # "staneee/aspnet:6-gdi-fontconfig"
 )
 
 # 创建编译器
