8c05a459adb05c291a499780269c4103fa537a4d
staneee <yi.hang@live.com>
2022-11-08 15:07:12 +0800

    修改buildx

--- build/build_and_push_imgs.ps1
+++ build/build_and_push_imgs.ps1
@@ -42,7 +42,7 @@ foreach ($path in $dockerFiles) {
     # 编译并推送镜像
     if ($buildX -contains $imgFullName) {
         # 多平台
-        docker buildx build . --force-rm --platform linux/arm64, linux/amd64 -t $imgFullName  -f ./Dockerfile --push
+        docker buildx build . --force-rm --platform linux/arm64,linux/amd64 -t $imgFullName  -f ./Dockerfile --push
     }
     else {
         # 单平台
