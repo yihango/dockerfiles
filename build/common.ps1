de650f843f78038e6e8e4288b0b38eac96e63481
staneee <yi.hang@live.com>
2022-11-09 18:06:52 +0800

    修改路径

--- build/common.ps1
+++ build/common.ps1
@@ -7,13 +7,13 @@ $buildX = (
     # "staneee/aspnet:6-focal-gdi-fontconfig",
     # "staneee/dotnet:5-focal",
     # "staneee/dotnet:6-focal",
-    # "staneee/powershell:lts-debian-10-focal",
-    # "staneee/powershell:lts-debian-10-focal-docker-20-10-21",
     # "staneee/nginx:1.19.6-basic",
     # "staneee/nginx:1.19.6-appconfig-prod",
     # "staneee/nginx:1.19.6-shell-runner",
-    # "staneee/nginx:1.19.6-wait-for-it"
-    # "staneee/node:16.13.1"
+    # "staneee/nginx:1.19.6-wait-for-it",
+    # "staneee/node:16.13.1",
+    # "staneee/powershell:lts-debian-10-focal",
+    # "staneee/powershell:lts-debian-10-focal-docker-20-10-21",    
     # "staneee/powershell:lts-debian-10-focal-dotnet-5",
     # "staneee/powershell:lts-debian-10-focal-dotnet-6",
     # "staneee/powershell:lts-debian-10-focal-node-14",
