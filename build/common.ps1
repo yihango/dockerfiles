dd69d90c16fd49233228de67c414be471a16b204
staneee <yi.hang@live.com>
2022-11-09 14:16:36 +0800

    新增编译项

--- build/common.ps1
+++ build/common.ps1
@@ -1,15 +1,15 @@
 # 需要多平台编译
 $buildX = (
-    # "staneee/aspnet:5-focal-gdi-fontconfig",
-    # "staneee/aspnet:6-focal-gdi-fontconfig",
-    # "staneee/dotnet:5-focal",
-    # "staneee/dotnet:6-focal",
-    # "staneee/powershell:lts-debian-10-focal"
-    "staneee/powershell:lts-debian-10-focal-docker-20-10-21"
-    # "staneee/nginx:1.19.6-basic"
-    # "staneee/nginx:1.19.6-appconfig-prod",
-    # "staneee/nginx:1.19.6-shell-runner",
-    # "staneee/nginx:1.19.6-wait-for-it"
+    "staneee/aspnet:5-focal-gdi-fontconfig",
+    "staneee/aspnet:6-focal-gdi-fontconfig",
+    "staneee/dotnet:5-focal",
+    "staneee/dotnet:6-focal",
+    "staneee/powershell:lts-debian-10-focal",
+    "staneee/powershell:lts-debian-10-focal-docker-20-10-21",
+    "staneee/nginx:1.19.6-basic",
+    "staneee/nginx:1.19.6-appconfig-prod",
+    "staneee/nginx:1.19.6-shell-runner",
+    "staneee/nginx:1.19.6-wait-for-it"
 )
 
 # 普通编译
