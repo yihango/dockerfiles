545e31c6d4ab872b13441f2ca87fa097093f9992
staneee <yi.hang@live.com>
2022-11-09 09:20:17 +0800

    增加6-focal-gdi-fontconfig

--- build/common.ps1
+++ build/common.ps1
@@ -1,6 +1,6 @@
 # 需要打包的
 $needBuild = (
-    ""
+    "staneee/aspnet:6-focal-gdi-fontconfig"
     # "staneee/powershell:lts-debian-10-docker-20-10-21-arm64"
     # "staneee/aspnet:5-gdi-fontconfig-arm64v8",
     # "staneee/aspnet:6-gdi-fontconfig"
@@ -8,7 +8,7 @@ $needBuild = (
 
 # 多平台编译
 $buildX = (
-    ""
+    "staneee/aspnet:6-focal-gdi-fontconfig"
 )
 
 # 创建编译器
---
+++ src/aspnet/6-focal-gdi-fontconfig/build.ps1
@@ -0,0 +1,2 @@
+docker build . --force-rm -t staneee/aspnet:6-focal-gdi-fontconfig  -f ./Dockerfile
+docker push staneee/aspnet:6-focal-gdi-fontconfig
