a7bcc1e8a05f8e5872ae05e73c295be836127609
staneee <yi.hang@live.com>
2022-11-09 15:28:07 +0800

    nodejs

--- build/common.ps1
+++ build/common.ps1
@@ -3,16 +3,17 @@ $currentPath = (Get-Location).Path
 
 # 需要多平台编译
 $buildX = (
-    "staneee/aspnet:5-focal-gdi-fontconfig",
-    "staneee/aspnet:6-focal-gdi-fontconfig",
-    "staneee/dotnet:5-focal",
-    "staneee/dotnet:6-focal",
-    "staneee/powershell:lts-debian-10-focal",
-    "staneee/powershell:lts-debian-10-focal-docker-20-10-21",
-    "staneee/nginx:1.19.6-basic",
-    "staneee/nginx:1.19.6-appconfig-prod",
-    "staneee/nginx:1.19.6-shell-runner",
-    "staneee/nginx:1.19.6-wait-for-it"
+    # "staneee/aspnet:5-focal-gdi-fontconfig",
+    # "staneee/aspnet:6-focal-gdi-fontconfig",
+    # "staneee/dotnet:5-focal",
+    # "staneee/dotnet:6-focal",
+    # "staneee/powershell:lts-debian-10-focal",
+    # "staneee/powershell:lts-debian-10-focal-docker-20-10-21",
+    # "staneee/nginx:1.19.6-basic",
+    # "staneee/nginx:1.19.6-appconfig-prod",
+    # "staneee/nginx:1.19.6-shell-runner",
+    # "staneee/nginx:1.19.6-wait-for-it"
+    "staneee/node:16.13.1"
 )
 
 # 普通编译
