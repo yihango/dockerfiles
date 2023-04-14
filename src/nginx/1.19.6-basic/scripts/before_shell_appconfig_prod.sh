d1019a183b2e8d7c3ea0c54d5760d19b8190bce8
staneee <yi.hang@live.com>
2023-04-14 09:32:30 +0800

    nginx脚本增加值不为空判断

--- src/nginx/1.19.6-basic/scripts/before_shell_appconfig_prod.sh
+++ src/nginx/1.19.6-basic/scripts/before_shell_appconfig_prod.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 # create before_shell.sh
-cat >/usr/share/nginx/html/assets/appconfig.prod.json <<EOF
+if [ -n "${APPCONFIG}" ]; then
+  cat <<EOF > /usr/share/nginx/html/assets/appconfig.prod.json
 ${APPCONFIG}
 EOF
+fi
