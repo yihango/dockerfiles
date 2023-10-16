a543a363e96e809a879bd15f92081dc4b8ee6741
stan e <yi.hang@live.com>
2023-10-16 22:40:03 +0800

    修改link创建

--- build/common.ps1
+++ build/common.ps1
@@ -3,8 +3,8 @@ $currentPath = (Get-Location).Path
 
 # 需要多平台编译
 $buildX = (
-    "staneee/aspnet:5-focal-gdi-fontconfig",
     "staneee/aspnet:6-focal-gdi-fontconfig",
+    "staneee/aspnet:5-focal-gdi-fontconfig",
     # "staneee/aspnet:5-focal-puppeteer", # 未完成
     # "staneee/aspnet:6-focal-puppeteer", # 未完成
     # "staneee/dotnet:5-focal",
