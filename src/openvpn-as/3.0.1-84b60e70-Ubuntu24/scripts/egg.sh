1bc37788869dfd6f1bc10c80daf82ba8a5863dbd
yihango <yi.hang@live.com>
2025-11-20 21:35:27 +0800

    增加额外的脚本

---
+++ src/openvpn-as/3.0.1-84b60e70-Ubuntu24/scripts/egg.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#
+source_dir="/openvpn/egg"
+target_dir="/usr/local/openvpn_as/lib/python"
+
+#
+if [ ! -d "$source_dir" ]; then
+    echo "error: not found $source_dir"
+    exit 1
+fi
+
+#
+if [ ! -d "$target_dir" ]; then
+    echo "error: not found $target_dir"
+    exit 1
+fi
+
+# find first .egg
+first_egg_file=$(find "$source_dir" -maxdepth 1 -name "*.egg" -type f | head -n 1)
+
+if [ -z "$first_egg_file" ]; then
+    echo "error: not found file .egg "
+    exit 0
+fi
+
+echo "info: .egg file $first_egg_file"
+echo "info: copy to $target_dir"
+
+# copy file
+cp -f "$first_egg_file" "$target_dir/"
+
+if [ $? -eq 0 ]; then
+    echo "success: copy success $(basename "$first_egg_file")"
+else
+    echo "error: copy fail $(basename "$first_egg_file")"
+    exit 1
+fi
