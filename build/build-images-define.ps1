e8caa0eedda47f19eec2d373300a5b1273e370bd
yihango <yi.hang@live.com>
2025-11-20 09:43:01 +0800

    增加 wg-easy 镜像

--- build/build-images-define.ps1
+++ build/build-images-define.ps1
@@ -151,7 +151,8 @@ $buildImageList = @(
     # "nextcloud:31-fpm",
     # "next-terminal:v2.6.5",
     # "guacd:1.6.0",
-    "ddns-go:v6.13.2",
+    # "ddns-go:v6.13.2",
+    "wg-easy:15.1.0",
     ""
 )
 
