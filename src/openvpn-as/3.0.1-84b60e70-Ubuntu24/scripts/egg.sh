3d3223dc67c40f6e06e5db368e4e55d6ab8b8116
yihango <yi.hang@live.com>
2025-11-20 22:28:13 +0800

    优化egg.sh

--- src/openvpn-as/3.0.1-84b60e70-Ubuntu24/scripts/egg.sh
+++ src/openvpn-as/3.0.1-84b60e70-Ubuntu24/scripts/egg.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
 
-#
+# 复制.egg文件的逻辑
 source_dir="/openvpn/egg"
 target_dir="/usr/local/openvpn_as/lib/python"
 
-#
-if [ ! -d "$source_dir" ]; then
-    mkdir "$source_dir"
-fi
-
-#
-if [ ! -d "$target_dir" ]; then
-    echo "error: not found $target_dir"
-    exit 0
-fi
-
-# find first .egg
-first_egg_file=$(find "$source_dir" -maxdepth 1 -name "*.egg" -type f | head -n 1)
-
-if [ -z "$first_egg_file" ]; then
-    echo "error: not found file .egg "
-    exit 0
-fi
-
-echo "info: .egg file $first_egg_file"
-echo "info: copy to $target_dir"
-
-# copy file
-cp -f "$first_egg_file" "$target_dir/"
-
-if [ $? -eq 0 ]; then
-    echo "success: copy success $(basename "$first_egg_file")"
-else
-    echo "error: copy fail $(basename "$first_egg_file")"
-    exit 0
+if [ -d "$source_dir" ] && [ -d "$target_dir" ]; then
+    first_egg_file=$(find "$source_dir" -maxdepth 1 -name "*.egg" -type f | head -n 1)
+    if [ -n "$first_egg_file" ]; then
+        echo "info: copying .egg file: $first_egg_file"
+        cp -f "$first_egg_file" "$target_dir/" 2>/dev/null && echo "success: copied successfully" || echo "error: copy failed"
+    fi
 fi
